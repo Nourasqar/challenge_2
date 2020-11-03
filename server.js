448b10906b165489bf967196a4934a1615f8a534
Nourasqar <noorashqar123@gmail.com>
2020-11-03 20:01:42 +0200

    challenge 2......

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+var port = 3000
+app.get('/',(req,res)=>{
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname,'/client/index.html'))
+})
+app.post('/',(req,res)=>{
+    res.render('app.js')
+})
+app.listen(port,function(){
+    console.log("im here xd")
+})
 
 
